ed4a25e38397b0597b2633d339eec42005abde02
Georgeappau <app21005@byui.edu>
2023-09-18 15:59:21 -0700

    Name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your name?");
+        string LastName = Console.ReadLine();
+        string FirstName = Console.ReadLine();
+        Console.WriteLine($"{LastName},{FirstName} {LastName}");
     }
 }
