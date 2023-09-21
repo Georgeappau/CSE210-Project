25dfaed3dfd22f47d90ea2f80aae6c9e773d2871
Georgeappau <app21005@byui.edu>
2023-09-21 04:23:06 -0700

    Prep3 update

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+      Random randomNumber = new Random();
+      int magicNumber = randomNumber.Next(1,101);
+      
+      int guess = 0;
+      
+    while (guess != magicNumber)
+    {
+
+      Console.Write("What is the magic number? ");
+      string userGuess = Console.ReadLine();
+      guess = int.Parse(userGuess);
+
+      if (guess == magicNumber)
+      {
+        Console.WriteLine("You guessed right");
+      }
+      
+      else if (guess < magicNumber)
+      {
+        Console.WriteLine("Higher");  
+      }
+      else
+      {
+        Console.WriteLine("Lower");
+      }
+    }
     }
 }
+
